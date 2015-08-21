1fb4e6211cb75f7630b07c4f7e88e5a8273f687f
Reed Weichler <rweichler@gmail.com>
2015-08-20 21:32:03 -0700

    fix main control flow

--- main.lua
+++ main.lua
@@ -5,10 +5,8 @@ L.init(_ENV)
 local NSLog = L.make_func(L.C.dlsym("NSLog"), L.C.call.ptr2ptr)
 
 local window = UIWindow()
-window.Frame = {0,0,320,480}
+window.Frame = {0, 0, 320, 480}
 window.BackgroundColor = UIColor.BlueColor
-
-window:makeKeyAndVisible()
 
 local label = UILabel()
 label.Frame = {0, 20, 320, 40}
@@ -16,7 +14,13 @@ label.BackgroundColor = UIColor.RedColor
 label.TextColor = UIColor.WhiteColor
 label.Text = NSString{WithUTF8String = tostring(getmetatable(_ENV))}
 
-window:add{Subview = label}
+local controller = UIViewController()
+controller.View.Frame = {0, 0, 320, 480}
+controller.View:add{Subview = label}
+
+window.RootViewController = controller
+
+window:makeKeyAndVisible()
 
 --print out the errors on the iPhone
 --live code updating
