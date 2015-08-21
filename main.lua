65923f627d399361cdfa52fe301c3e6ceb9d10bd
Reed Weichler <rweichler@gmail.com>
2015-08-20 21:54:35 -0700

    main

--- main.lua
+++ main.lua
@@ -2,24 +2,21 @@ local L = require 'luikit'
 
 L.init(_ENV)
 
-local NSLog = L.make_func(L.C.dlsym("NSLog"), L.C.call.ptr2ptr)
-
-local window = UIWindow()
-window.Frame = {0, 0, 320, 480}
-window.BackgroundColor = UIColor.BlueColor
 
 local label = UILabel()
 label.Frame = {0, 20, 320, 40}
-label.BackgroundColor = UIColor.RedColor
+label.BackgroundColor = UIColor.BlackColor
 label.TextColor = UIColor.WhiteColor
 label.Text = NSString{WithUTF8String = tostring(getmetatable(_ENV))}
 
 local controller = UIViewController()
 controller.View.Frame = {0, 0, 320, 480}
 controller.View:add{Subview = label}
+controller.View.BackgroundColor = UIColor.BlueColor
 
+local window = UIWindow()
+window.Frame = {0, 0, 320, 480}
 window.RootViewController = controller
-
 window:makeKeyAndVisible()
 
 --print out the errors on the iPhone
