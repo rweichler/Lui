c4efad5d0b33251102b5642598bfac46c36943a4
Reed Weichler <rweichler@gmail.com>
2015-08-23 15:14:03 -0700

    messing around with struct possibilities

--- type_conversion/CGRect.c
+++ type_conversion/CGRect.c
@@ -1,22 +1,39 @@
 #include <lua/lua.h>
 #include <CoreGraphics/CoreGraphics.h>
 
-static int lua_to_c(lua_State *L)
+const char *NAME = "CGRect";
+
+int lua_to_c(lua_State *L)
 {
-    CGRect *ptr = malloc(sizeof(CGRect));
+    CGRect *ptr = lua_newuserdata(L, sizeof(CGRect));
     *ptr = CGRectMake(
                 lua_tonumber(L, 1),
                 lua_tonumber(L, 2),
                 lua_tonumber(L, 3),
                 lua_tonumber(L, 4)
            );
-
-    lua_pushlightuserdata(L, ptr);
-
     return 1;
 }
 
-static int c_to_lua(lua_State *L)
+#define FUNC(ORIGIN, X)                         \
+static int get_##X(lua_State *L)                \
+{                                               \
+    CGRect *ptr = lua_touserdata(L, 1);         \
+    if(lua_gettop(L) == 1) { /* get */          \
+        lua_pushnumber(L, ptr->ORIGIN.X);       \
+        return 1;                               \
+    } else {                 /* set */          \
+        ptr->ORIGIN.X = lua_tonumber(L, 2);     \
+        return 0;                               \
+    }                                           \
+}
+
+FUNC(origin, x);
+FUNC(origin, y);
+FUNC(size, width);
+FUNC(size, height);
+
+int c_to_lua(lua_State *L)
 {
     CGRect *ptr = lua_touserdata(L, 1);
     CGRect rect = *ptr;
@@ -27,4 +44,9 @@ static int c_to_lua(lua_State *L)
     lua_pushnumber(L, rect.size.height);
 
     return 4;
+}
+
+int register_metatable(lua_State *L)
+{
+    
 }
--- type_conversion/CGRect.lua
+++ type_conversion/CGRect.lua
@@ -3,16 +3,40 @@ function lua_to_c(tbl)
     return C.lua_to_c(table.unpack(tbl))
 end
 
-
 local map = {
-    x = 1,
-    y = 2,
-    width = 3,
-    height = 4
+    "x",
+    "y",
+    "width",
+    "height"
 }
 
 function c_to_lua(ptr)
     local result = table.pack(C.c_to_lua(ptr))
     mapfields(result, map)
     return result
+end
+
+
+--funcmap is {x(), y(), width(), height()}
+function get_metatable(funcmap)
+    for i = 1, 4 do
+        funcmap[map[i]] = funcmap[i]
+    end
+
+    return {
+        __index = function(self, key)
+            local func = funcmap[key]
+            if func then
+                return func(self)
+            end
+        end,
+        __newindex = function(self, key, value)
+            local func = funcmap[key]
+            if func then
+                return func(self, value)
+            else
+                error("set invalid field")
+            end
+        end
+    }
 end
