aa09b4526d7a837a6b367557343470db0ec6f7aa
Reed Weichler <rweichler@gmail.com>
2015-08-27 16:42:19 -0700

    better check for args in CGRect set/get

--- type_conversion/CGRect.c
+++ type_conversion/CGRect.c
@@ -15,26 +15,20 @@ int lua_to_c(lua_State *L)
     return 1;
 }
 
-static int set_thishit(lua_State *L, CGFloat *val)
-{
-    CGRect *ptr = lua_touserdata(L, 1);
-    switch(lua_gettop(L)) {
-        case 1: //get
-            lua_pushnumber(L, *val);
-            return 1;
-        case 2: //set
-            *val = lua_tonumber(L, 2);
-            return 0;
-        default:
-            return luaL_error("invalid # of arguments (expected 1 or 2)");
-    }
-}
-
-#define FUNC(ORIGIN, X)                         \
-static int get_##X(lua_State *L)                \
-{                                               \
-    CGRect *ptr = lua_touserdata(L, 1);         \
-    return set_thishit(L, &(ptr->ORIGIN.X));    \
+#define FUNC(ORIGIN, X)                             \
+static int get_##X(lua_State *L)                    \
+{                                                   \
+    switch(lua_gettop(L)) {                         \
+        case 1: /* get */                           \
+            CGRect *ptr = lua_touserdata(L, 1);     \
+            lua_pushnumber(L, ptr->ORIGIN.X);       \
+            return 1;                               \
+        case 2: /* set */                           \
+            CGRect *ptr = lua_touserdata(L, 1);     \
+            ptr->ORIGIN.X = lua_tonumber(L, 2);     \
+            return 0;                               \
+    }                                               \
+    return luaL_error("invalid # of arguments");    \
 }
 
 FUNC(origin, x);
